d03ee260e1f97885358b93f418bac54ffb4e0821
amn93p <abbenfarhat@gmail.com>
2024-03-15 12:28:41 +0100

    derniere mise à jour

--- src/Views/home.view.php
+++ src/Views/home.view.php
@@ -3,13 +3,15 @@ require_once(__DIR__ . '/../../src/Controllers/HomeController.php');
 $title = 'Accueil';
 require_once(__DIR__ . "/parts/head.php");
 ?>
-<h1>Hello 👌</h1>
+<h1 class="text-center">Hello 👌</h1>
 <?php
 foreach ($myListUsers as $user) {
 ?>
+<div class="text-center">
     <p><?= $user->username ?></p>
     <p><?= $user->email ?></p>
     <p><?= $user->id ?></p>
+</div>
 <?php
 }
 require_once(__DIR__ . "/parts/footer.php");
--- src/Views/parts/footer.php
+++ src/Views/parts/footer.php
@@ -1,4 +1,4 @@
-<footer class="bg-body-tertiary text-center text-lg-start mt-5">
+<footer class="bg-body-tertiary text-center text-lg-start mt-5 fixed-bottom">
     <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.05);">
         © 2024 amine
     </div>
